7a014061047ae4b87ad02c4500de36a4de7a4be9
尹昊萱 <yhx18@mails.tsinghua.edu.cn>
2020-09-11 19:27:31 +0800

    finish entity html

--- app/src/main/java/com/example/covidapplication/DataFragment.java
+++ app/src/main/java/com/example/covidapplication/DataFragment.java
@@ -34,36 +34,6 @@ public class DataFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_data, container, false);
-        BarChart mCountryBar = view.findViewById(R.id.country_bar);
-        ArrayList<Place> countryList = MainActivity.placeManager.countryList;
-        Log.d("mylist", Integer.toString(countryList.size()));
-        Log.d("mainlist", Integer.toString(MainActivity.placeManager.countryList.size()));
-        ArrayList<BarEntry> countryBarList = new ArrayList<>();
-        int i = 0;
-        for (Place country : countryList)
-        {
-
-//            if (country.name == "China" || country.name == "India") {
-            countryBarList.add(new BarEntry(country.confirmed.get(country.confirmed.size() - 1), ++i));
-//            }
-        }
-        Log.d("countrylist", Integer.toString(countryList.size()));
-        Log.d("countrybarlist", Integer.toString(countryBarList.size()));
-        BarDataSet countryBarSet = new BarDataSet(countryBarList, "Country");
-        BarData countryData = new BarData(countryBarSet);
-        mCountryBar.setData(countryData);
-        mCountryBar.invalidate();
-
-        BarChart mProvinceBar = view.findViewById(R.id.province_bar);
-        ArrayList<Place> provinceList = MainActivity.placeManager.provinceList;
-        ArrayList<BarEntry> provinceBarList = new ArrayList<>();
-        int j = 0;
-        for (Place province : provinceList) {
-            provinceBarList.add(new BarEntry(province.confirmed.get(province.confirmed.size() - 1), ++j));
-        }
-        BarData provinceData = new BarData(new BarDataSet(provinceBarList, "Province"));
-        mProvinceBar.setData(provinceData);
-        mProvinceBar.invalidate();
         return view;
     }
 }
--- app/src/main/java/com/example/covidapplication/EntityActivity.java
+++ app/src/main/java/com/example/covidapplication/EntityActivity.java
@@ -2,6 +2,8 @@ package com.example.covidapplication;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.text.Html;
+import android.text.SpannableString;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,18 +22,18 @@ public class EntityActivity extends AppCompatActivity {
         TextView nameView = findViewById(R.id.entity_name);
         nameView.setText(entity.label);
         TextView hotView = findViewById(R.id.entity_hot);
-        hotView.setText("热度: " + entity.hot);
+        hotView.setText(Html.fromHtml("<b>热度: </b>" + entity.hot, Html.FROM_HTML_MODE_COMPACT));
         TextView urlView = findViewById(R.id.entity_url);
-        urlView.setText("Url: " + entity.url);
+        urlView.setText(Html.fromHtml("<b>Url: </b>" + entity.url, Html.FROM_HTML_MODE_COMPACT));
         TextView wikiView = findViewById(R.id.entity_wiki);
         if (entity.enwiki != null) {
-            wikiView.setText("百科: " + entity.enwiki);
+            wikiView.setText(Html.fromHtml("<b>百科: </b>" + entity.enwiki,Html.FROM_HTML_MODE_COMPACT));
         }
         else if (entity.baidu != null) {
-            wikiView.setText("百科: " + entity.baidu);
+            wikiView.setText(Html.fromHtml("<b>百科: </b>" + entity.baidu, Html.FROM_HTML_MODE_COMPACT));
         }
         else if (entity.zhwiki != null) {
-            wikiView.setText("百科: " + entity.zhwiki);
+            wikiView.setText(Html.fromHtml("<b>百科: </b>" + entity.zhwiki, Html.FROM_HTML_MODE_COMPACT));
         }
         else {
             wikiView.setText("No wiki available");
@@ -44,14 +46,14 @@ public class EntityActivity extends AppCompatActivity {
         TextView propertyView = findViewById(R.id.entity_properties);
         StringBuilder properties = new StringBuilder();
         for (Map.Entry<String, String> entry: entity.properties.entrySet()) {
-            properties.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            properties.append("<b>").append(entry.getKey()).append("</b> ").append(entry.getValue()).append("<br>");
         }
-        propertyView.setText(properties.toString());
+        propertyView.setText(Html.fromHtml(properties.toString(),Html.FROM_HTML_MODE_COMPACT));
         TextView relationView = findViewById(R.id.entity_relations);
         StringBuilder relations = new StringBuilder();
         for (Relation relation : entity.relations) {
-            relations.append("名称：").append(relation.label).append("\n关系：").append(relation.relation).append("\nurl: ").append(entity.url).append("\n方向：").append(relation.forward ? "向前\n" : "向后\n");
+            relations.append("<b>名称：</b>").append(relation.label).append("<br><b>关系：</b>").append(relation.relation).append("<br><b>url: </b>").append(entity.url).append("<br><b>方向：</b>").append(relation.forward ? "向前" : "向后").append("<br><br>");
         }
-        relationView.setText(relations.toString());
+        relationView.setText(Html.fromHtml(relations.toString(),Html.FROM_HTML_MODE_COMPACT));
     }
 }
