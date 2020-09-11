b520b43697deac54d50d5ddef8db4472d5ca2e4e
尹昊萱 <yhx18@mails.tsinghua.edu.cn>
2020-09-11 20:41:43 +0800

    really finish avatar

--- app/src/main/java/com/example/covidapplication/ScholarActivity.java
+++ app/src/main/java/com/example/covidapplication/ScholarActivity.java
@@ -1,6 +1,7 @@
 package com.example.covidapplication;
 
 import android.content.Intent;
+import android.text.Html;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,28 +24,28 @@ public class ScholarActivity extends AppCompatActivity {
         TextView affiliationView = findViewById(R.id.affiliation);
         affiliationView.setText(scholar.affiliation);
         TextView citationView = findViewById(R.id.citation);
-        citationView.setText("Citations: " + scholar.citations);
+        citationView.setText(Html.fromHtml("<b>Citations: </b>" + scholar.citations, Html.FROM_HTML_MODE_COMPACT));
         TextView gindexView = findViewById(R.id.gindex);
-        gindexView.setText("G-index: " + scholar.gindex);
+        gindexView.setText(Html.fromHtml("<b>G-index: </b>" + scholar.gindex, Html.FROM_HTML_MODE_COMPACT));
         TextView hindexView = findViewById(R.id.hindex);
-        hindexView.setText("H-index: " + scholar.hindex);
+        hindexView.setText(Html.fromHtml("<b>H-index: </b>" + scholar.hindex, Html.FROM_HTML_MODE_COMPACT));
         TextView followedView = findViewById(R.id.followed);
-        followedView.setText("Number of followed: " + scholar.num_followed);
+        followedView.setText(Html.fromHtml("<b>Number of followed: </b>" + scholar.num_followed, Html.FROM_HTML_MODE_COMPACT));
         TextView viewedView = findViewById(R.id.viewed);
-        viewedView.setText("Number of viewed: " + scholar.num_viewed);
+        viewedView.setText(Html.fromHtml("<b>Number of viewed: </b>" + scholar.num_viewed, Html.FROM_HTML_MODE_COMPACT));
         TextView activityView = findViewById(R.id.activity);
-        activityView.setText("Activity: " + scholar.activity);
+        activityView.setText(Html.fromHtml("<b>Activity: </b>" + scholar.activity, Html.FROM_HTML_MODE_COMPACT));
         TextView diversityView = findViewById(R.id.diversity);
-        diversityView.setText("Diversity: " + scholar.diversity);
+        diversityView.setText(Html.fromHtml("<b>Diversity: </b>" + scholar.diversity, Html.FROM_HTML_MODE_COMPACT));
         TextView newStarView = findViewById(R.id.new_star);
-        newStarView.setText("New Star: " + scholar.newStar);
+        newStarView.setText(Html.fromHtml("<b>New Star: </b>" + scholar.newStar, Html.FROM_HTML_MODE_COMPACT));
         TextView sociabilityView = findViewById(R.id.sociability);
-        sociabilityView.setText("Sociability: " + scholar.sociability);
+        sociabilityView.setText(Html.fromHtml("<b>Sociability: </b>" + scholar.sociability, Html.FROM_HTML_MODE_COMPACT));
         TextView bioView = findViewById(R.id.bio);
-        bioView.setText(scholar.bio);
+        bioView.setText(Html.fromHtml(scholar.bio, Html.FROM_HTML_MODE_COMPACT));
         TextView eduView = findViewById(R.id.edu);
-        eduView.setText(scholar.edu);
+        eduView.setText(Html.fromHtml(scholar.edu, Html.FROM_HTML_MODE_COMPACT));
         TextView workView = findViewById(R.id.work);
-        workView.setText(scholar.work);
+        workView.setText(Html.fromHtml(scholar.work, Html.FROM_HTML_MODE_COMPACT));
     }
 }
