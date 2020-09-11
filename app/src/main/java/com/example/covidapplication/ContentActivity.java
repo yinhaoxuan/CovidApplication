5684351a321e530669df3c319f0445656227d81c
尹昊萱 <yhx18@mails.tsinghua.edu.cn>
2020-09-11 14:21:09 +0800

    sync

--- app/src/main/java/com/example/covidapplication/ContentActivity.java
+++ app/src/main/java/com/example/covidapplication/ContentActivity.java
@@ -1,6 +1,8 @@
 package com.example.covidapplication;
 
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,5 +22,16 @@ public class ContentActivity extends AppCompatActivity {
         timeView.setText(intent.getStringExtra("time"));
         TextView contentView = findViewById(R.id.content);
         contentView.setText(intent.getStringExtra("content"));
+        Button button = findViewById(R.id.share);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, intent.getStringExtra("title"));
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, "Share to..."));
+            }
+        });
     }
 }
